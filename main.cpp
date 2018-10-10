8feeee1821c303aaff23e585be09de56380d903e
jestice <okrassavaso@gmail.com>
2018-10-11 00:35:06 +0300

    vichitanie'

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sdafx.h>
+using namespace std;
+
+int main()
+
+{
+	int a;
+
+	int b;
+
+	printf("Vvedi 2 4isla \n");
+
+	scanf("%u",&a);
+
+	scanf("%u",&b);
+
+	printf("%u \n",a - b);
+	
+	
+	
+   return 0;
+
+}
